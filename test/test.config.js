10bc9dca6e1704937ffe115d046f67f5ac9eb619
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-12-11 11:09:15 +0100

    Basic test setup

---
+++ test/test.config.js
@@ -0,0 +1,13 @@
+/*jshint expr:true */
+'use strict';
+
+var should = require('should');
+
+describe('Load the application configuration', function() {
+  it('should return an object containing a port and basepath', function(done) {
+    var appConfig = require('../lib/config').app();
+    appConfig.should.have.property('port');
+    appConfig.should.have.property('basepath');
+    done();
+  });
+});
