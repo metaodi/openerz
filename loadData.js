17135efc7e7867fb1cd2d74fd7cfbca4cc08e6da
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-12-20 13:18:27 +0100

    Refactoring of loadData to reduce duplicate code

--- loadData.js
+++ loadData.js
@@ -18,123 +18,97 @@ db.on('ready', function() {
     db.station.remove();
     db.calendar.remove();
 
-    async.series([
-        function(callback){
-            csv.convertToJson('./csv/Entsorgung_Sammelstellen_2014.csv', format.stationEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
+    var importCsv = function(path, format, collection, type, callback) {
+        csv.convertToJson(path, format, function (objArr) {
+            console.log('CSV converted, got ' + objArr.length + ' objects');
 
-                _.each(objArr, function (obj) {
-                    db.station.insert(obj);
-                });
-                console.log("Finished inserting stations!");
-                callback(null, "stations");
+            _.each(objArr, function (obj) {
+                if (type !== 'stations') {
+                    obj['type'] = type;
+                }
+                collection.insert(obj);
             });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Bioabfall_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
+            console.log("Finished inserting", type);
+            callback();
+        });
+    }
 
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'organic';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for organic waste!");
-                callback(null, "organic waste");
-            });
+    var csvs = [
+        {
+            'path': './csv/Entsorgung_Sammelstellen_2014.csv',
+            'format': format.stationEntry,
+            'collection': db.station,
+            'type': 'stations'
         },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_CargoTram_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
+        {
+            'path': './csv/Entsorgungskalender_Bioabfall_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'organic'
+        },
+        {
+            'path': './csv/Entsorgungskalender_CargoTram_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'cargotram'
+        },
+        {
+            'path': './csv/Entsorgungskalender_eTram_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'etram'
+        },
+        {
+            'path': './csv/Entsorgungskalender_Karton_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'cardboard'
+        },
+        {
+            'path': './csv/Entsorgungskalender_Papier_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'paper'
+        },
+        {
+            'path': './csv/Entsorgungskalender_Kehricht_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'waste'
+        },
+        {
+            'path': './csv/Entsorgungskalender_Sonderabfall_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'special'
+        },
+        {
+            'path': './csv/Entsorgungskalender_Textilien_2014.csv',
+            'format': format.calendarEntry,
+            'collection': db.calendar,
+            'type': 'textile'
+        },
+    ];
 
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'cargotram';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar of cargo tram!");
-                callback(null, "cargo tram");
-            });
+    async.eachSeries(
+        csvs,
+        function (csv, callback) {
+            try {
+                importCsv(csv.path, csv.format, csv.collection, csv.type, callback);
+            } catch (e) {
+                callback(e);
+            }
         },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_eTram_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'etram';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar of e-tram!");
-                callback(null, "e-tram");
-            });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Karton_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'cardboard';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for cardboard waste!");
-                callback(null, "cardboard");
-            });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Papier_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'paper';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for paper waste!");
-                callback(null, "paper");
-            });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Kehricht_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'waste';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for general waste!");
-                callback(null, "waste");
-            });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Sonderabfall_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'special';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for special waste!");
-                callback(null, "special");
-            });
-        },
-        function(callback){
-            csv.convertToJson('./csv/Entsorgungskalender_Textilien_2014.csv', format.calendarEntry, function (objArr) {
-                console.log('CSV converted, got ' + objArr.length + ' objects');
-
-                _.each(objArr, function (obj) {
-                    obj['type'] = 'textile';
-                    db.calendar.insert(obj);
-                });
-                console.log("Finished inserting calendar for textile waste!");
-                callback(null, "cargo tram");
-            });
-        },
-    ],
-    function(err, results){
-        if (err) {
-            console.log("Error occured:", err);
-            process.exit(code=1);
+        function(err){
+            if (err) {
+              console.log('Import failed:', err);
+                process.exit(code=1);
+            } else {
+                console.log("Import finished");
+                process.exit(code=0);
+            }
         }
-        console.log("Finished importing", results);
-        process.exit(code=0);
-    });
+    ); 
 });
 
 // make sure the database in opened and the 'ready' event is fired
