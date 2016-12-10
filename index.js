997f853e54a0a7fe6e95891147dfc38bc245d8a4
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-12-10 06:11:47 +0100

    Update year to 2017

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var swaggerOptions = {
     info: {
         title: 'Open ERZ API',
         description: '<ul style=\'list-style-type: disc; padding: 20px; font-size: 16px;\'><li>This API provides data from <a href=\'https://www.stadt-zuerich.ch/ted/de/index/entsorgung_recycling.html\'>Entsorgung und Recycling Zürich (ERZ)</a></<li><li>The data is provided on the <a href=\'https://data.stadt-zuerich.ch/\'>open data portal of the City of Zurich</a></li><li>All the code of <a href=\'https://github.com/metaodi/openerz\'>OpenERZ is open source and can be found on GitHub</a></li><li>The logo was created by sigit milshtein from the Noun Project (CC-BY 3.0)</li></ul>',
-        license: 'MIT License, © Stefan Oderbolz 2016',
+        license: 'MIT License, © Stefan Oderbolz 2017',
         licenseUrl: 'https://github.com/metaodi/openerz/blob/master/LICENSE.md'
     }
 };
