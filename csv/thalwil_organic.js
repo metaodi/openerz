746e82e337e6e4019c296ef0c68439ff8bcc3b2f
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-12-30 13:23:35 +0100

    Add new data for 2019

--- csv/thalwil_organic.js
+++ csv/thalwil_organic.js
@@ -2,13 +2,14 @@ var _ = require('underscore');
 var Moment = require('moment');
 
 // define day of the organic collection
+var startOfNextYear = Moment().add(1, 'year').startOf('year');
 var day = 'Wednesday';
-var organicDate = Moment().startOf('year').isoWeekday(day);
+var organicDate = Moment(startOfNextYear).isoWeekday(day);
 
 //CSV Header
 console.log("PLZ,Abholdatum");
 
-while (organicDate.year() <= Moment().year()) {
+while (organicDate.year() <= startOfNextYear.year()) {
     if (organicDate.month() !== 11) {
         console.log("8800," + organicDate.format('YYYY-MM-DD'));
     }
@@ -16,5 +17,5 @@ while (organicDate.year() <= Moment().year()) {
 }
 
 //special handling for december
-console.log("8800,2018-12-05");
-console.log("8800,2018-12-19");
+console.log("8800,2019-12-04");
+console.log("8800,2019-12-18");
