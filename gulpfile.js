d7d861ecf5acd611c0367d6407d42e3a2f200861
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2019-01-19 22:29:45 +0100

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp   = require('gulp');
 var gutil   = require('gulp-util');
 var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
 var jscs = require('gulp-jscs');
+var eslint = require('gulp-eslint');
 var exec = require('child_process').exec;
 
 var scripts = [
@@ -10,21 +10,32 @@ var scripts = [
     '!./node_modules/**/*.js'
 ];
 
-gulp.task('lint', function() {
+function oldlint() {
     return gulp.src(scripts)
         .pipe(jshint('./.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'))
         .pipe(jscs());
-});
+}
 
-gulp.task('test', function(cb) {
+function lint() {
+    return gulp.src(scripts)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+}
+
+function test(cb) {
     exec('npm test', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
-});
+}
 
-gulp.task('default', [ 'lint' ]);
-gulp.task('travis', [ 'lint', 'test' ]);
+gulp.task('lint', lint);
+gulp.task('oldlint', oldlint);
+gulp.task('test', test);
+
+gulp.task('default', lint );
+gulp.task('travis', gulp.series(lint, test));
