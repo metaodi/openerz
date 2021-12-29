dc8c903449907e66a115a56155ee1c14370c75f7
Stefan Oderbolz <oderbolz@gmail.com>
2021-12-29 17:48:24 +0100

    Update stgallen.py

--- csv/stgallen/stgallen.py
+++ csv/stgallen/stgallen.py
@@ -1,1 +1,33 @@
+import requests
+import csv
+from io import StringIO
 
+# Abfuhrtermine
+CSV_URL = "https://daten.stadt.sg.ch/explore/dataset/abfuhrdaten-stadt-stgallen/download/?format=csv&timezone=Europe/Zurich&lang=de&use_labels_for_header=false&csv_separator=%2C"
+r = requests.get(CSV_URL)
+reader = csv.DictReader(StringIO(r.text), delimiter=',')
+
+header = [
+    'termin',
+    'wochentag',
+    'art',
+    'zone',
+    'dayofweek',
+]
+
+with open('stgallen.csv', 'w') as f:
+    writer = csv.DictWriter(
+        f,
+        header,
+        delimiter=',',
+        quotechar='"',
+        lineterminator='\r\n',
+        quoting=csv.QUOTE_MINIMAL
+    )
+    writer.writeheader()
+
+    for row in reader:
+        print(row)
+        del row['geo_shape']
+        del row['geo_point_2d']
+        writer.writerow(row)
