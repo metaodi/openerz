0e8d95062e6e49dba63e683e56a5872ffeb8a0ef
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2023-01-04 13:39:21 +0100

    Add log to server

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 var liftOff = require('./lib/server').liftOff;
 
+const formatLogEvent = function(event) {
+    console.log(`[${event.tags}], ${event.data}`);
+};
+
+
 (async () => {
     try {
         var server = await liftOff();
         await server.start();
         console.log(`Server started at ${server.info.uri}`);
+        server.events.on('log', formatLogEvent);
 
     } catch (err) {
         console.log('Got an error while starting server: ' + err);
