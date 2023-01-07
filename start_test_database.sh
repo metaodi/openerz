93f40930cb1891a886e513dd43d83015590c3973
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-07 17:16:25 +0100

    Update start_test_database.sh

--- start_test_database.sh
+++ start_test_database.sh
@@ -8,7 +8,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 docker stop openerz-pg-test || true
 docker run -d --rm --name openerz-pg-test -e POSTGRES_PASSWORD="test-pw" -e POSTGRES_DB="postgres-test" -p 5433:5432 postgres:alpine
 
-sleep 2
+sleep 5
 
 source $DIR/.env.test
 psql $DATABASE_URL < $DIR/dump.sql
