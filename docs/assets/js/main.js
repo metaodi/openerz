6224f725e0bbe0a1de0d58cd2a113330918d1395
Stefan Oderbolz <oderbolz@gmail.com>
2023-01-11 16:22:29 +0100

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,33 @@
-console.log('This is main.js');
+window.addEventListener('DOMContentLoaded',function () {
+    'use strict';
+    var form = $$('#url-form')[0];                          
+    for (var i = 0; i < form.elements.length; i++) {
+        form.elements[i].onchange = function() {
+            var data = new FormData($$('#url-form')[0]);
+            updateUrl(data);
+        }
+    }
+}
+    
+});
+    
+function updateUrl(formData) {
+    // get all values
+    var gemeinde = data.get('gemeinde');
+    var art = data.getAll('art');
+    var area = data.get('area');
+    
+    if !(gemeinde) {
+        return;
+    }
+    var url = 'https://openerz.metaodi.ch/api/calendar.ics?lang=de&region=' + gemeinde;
+    if (area) {
+        url += '&area=' + area;
+    }
+    if (art) {
+        art.forEach.forEach(function (a) {
+            url += '&types=' + a;
+        });
+    }
+    return url;
+}
