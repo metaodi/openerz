b67fda836b33d2671a4fd0bf56555f2f1408144f
Stefan Oderbolz <oderbolz@gmail.com>
2025-01-06 14:55:10 +0100

    Add missing data

--- csv/wetzikon/wetzikon.py
+++ csv/wetzikon/wetzikon.py
@@ -4,6 +4,7 @@ import sys
 import os
 import re
 import logging
+import time
 from datetime import datetime, date, timedelta
 from openerzpy.download import download as dl
 from openerzpy.parse import parse_ics
@@ -76,38 +77,118 @@ source = [
         'waste_type': 'waste',
     },
     {  # Biogene Abfälle Kreis 2 
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=1343615&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=14256533&calhome=7215088',
         'area': '2',
         'waste_type': 'organic',
     },
     {  # Karton Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=3337955&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=3196426&calhome=7215088',
         'area': '2',
         'waste_type': 'cardboard',
     },
     {  # Häckseldienst Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=13387214&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=11850817&calhome=7215088',
         'area': '2',
         'waste_type': 'chipping_service',
     },
     {  # Grubengut Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=1993665&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=10531990&calhome=7215088',
         'area': '2',
         'waste_type': 'incombustibles',
     },
     {  # Metall Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=2292131&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=1240817&calhome=7215088',
         'area': '2',
         'waste_type': 'metal',
     },
      {  # Papier Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=7772360&calhome=7215088',
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=815845&calhome=7215088',
         'area': '2',
         'waste_type': 'paper',
     },
-     {  # Sonderabfall Kreis 2
-        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=13299769&calhome=7215088',
+    {  # Sonderabfall Kreis 2
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=3311361&calhome=7215088',
         'area': '2',
+        'waste_type': 'special',
+    },
+    {  # Abfall Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=8637784&calhome=7215088',
+        'area': '3',
+        'waste_type': 'waste',
+    },
+    {  # Biogene Abfälle Kreis 3 
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=9413633&calhome=7215088',
+        'area': '3',
+        'waste_type': 'organic',
+    },
+    {  # Karton Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=8165260&calhome=7215088',
+        'area': '3',
+        'waste_type': 'cardboard',
+    },
+    {  # Häckseldienst Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=12460691&calhome=7215088',
+        'area': '3',
+        'waste_type': 'chipping_service',
+    },
+    {  # Grubengut Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=793300&calhome=7215088',
+        'area': '3',
+        'waste_type': 'incombustibles',
+    },
+    {  # Metall Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=12639558&calhome=7215088',
+        'area': '3',
+        'waste_type': 'metal',
+    },
+     {  # Papier Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=1518902&calhome=7215088',
+        'area': '3',
+        'waste_type': 'paper',
+    },
+     {  # Sonderabfall Kreis 3
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=6292302&calhome=7215088',
+        'area': '3',
+        'waste_type': 'special',
+    },
+    {  # Abfall Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=10360829&calhome=7215088',
+        'area': '4',
+        'waste_type': 'waste',
+    },
+    {  # Biogene Abfälle Kreis 4 
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=5862926&calhome=7215088',
+        'area': '4',
+        'waste_type': 'organic',
+    },
+    {  # Karton Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=2013902&calhome=7215088',
+        'area': '4',
+        'waste_type': 'cardboard',
+    },
+    {  # Häckseldienst Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=6503446&calhome=7215088',
+        'area': '4',
+        'waste_type': 'chipping_service',
+    },
+    {  # Grubengut Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=13669829&calhome=7215088',
+        'area': '4',
+        'waste_type': 'incombustibles',
+    },
+    {  # Metall Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=2677237&calhome=7215088',
+        'area': '4',
+        'waste_type': 'metal',
+    },
+     {  # Papier Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=16272043&calhome=7215088',
+        'area': '4',
+        'waste_type': 'paper',
+    },
+    {  # Sonderabfall Kreis 4
+        'url': 'https://m.wetzikon.ch/appl/ical.php?apid=10819498&calhome=7215088',
+        'area': '4',
         'waste_type': 'special',
     },
 ]
@@ -133,7 +214,7 @@ try:
         end_date = datetime.fromisoformat(config['end_date'])
         events = parse_ics.parse_file(cal_path, start_date, end_date)
         for event in events:
-            print(event)
+            log.debug(event)
             if 'summary' not in event or not event['summary']:
                 continue
 
@@ -153,6 +234,7 @@ try:
             except KeyError:
                 print(f"ERROR: could not map {event['summary']}", file=sys.stderr)
                 raise
+        time.sleep(20)
 
     log.info("Start writing wetzikon.csv")
     csv_path = os.path.join(__location__, 'wetzikon.csv')
