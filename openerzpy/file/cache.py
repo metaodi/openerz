d5237f39ee2f89fae8ad788f00c248ad1399d908
Stefan Oderbolz <oderbolz@gmail.com>
2026-01-04 11:27:30 +0100

    Fix cache path

--- openerzpy/file/cache.py
+++ openerzpy/file/cache.py
@@ -5,10 +5,18 @@ import logging
 
 log = logging.getLogger(__name__)
 
+__location__ = os.path.realpath(
+    os.path.join(
+        os.getcwd(),
+        os.path.dirname(__file__)
+    )
+)
+
 
 def copy_file_from_cache(cache_path, output_path):
-    if not os.path.exists(cache_path):
-        raise FileNotFoundError(f"Cache path {cache_path} not found")
+    cache_loc = os.path.join(__location__, '..', '..', cache_path)
+    if not os.path.exists(cache_loc):
+        raise FileNotFoundError(f"Cache path {cache_loc} not found")
     log.debug(f"Copy file from cache {cache_path} to {output_path}")
-    shutil.copy(cache_path, output_path)
+    shutil.copy(cache_loc, output_path)
 
